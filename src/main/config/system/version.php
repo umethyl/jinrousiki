5eff6c8a7ccdbf5a74db4f515bed3d9246ad5a09
umethyl <235720316+umethyl@users.noreply.github.com>
2025-12-10 04:27:47 +0900

    [main] Ver. 5.2.0 α1

--- src/main/config/system/version.php
+++ src/main/config/system/version.php
@@ -4,10 +4,10 @@ final class ScriptInfo {
   const VERSION = 'Ver. 5.2.0 α1';
 
   //パッケージの最終更新日
-  const LAST_UPDATE = '2025/12/02 (Tue) 23:15';
+  const LAST_UPDATE = '2025/12/10 (Wed) 04:17';
 
   //パッケージ化した時の Revision
-  const REVISION = '#115';
+  const REVISION = '#131';
 
   //パッケージ名
   const PACKAGE = '<a href="https://github.com/umethyl/jinrousiki">人狼式</a>';
